678bcdd94bdfac2fabbb100f863fe174bbf96764
ciro matias <matiasciros666@gmail.com>
2026-04-30 11:58:30 -0300

    actualizacion30-4-26

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 console.log("hola mundo");
-alert("funciona");
+//alert("funciona");
 
 const Boton = document.getElementById("Btn");
 const Body = document.body;
 Boton.addEventListener ("click", ()=>{
     Body.classList.toggle("dark");
 });
+
