3029a28648df40b1e191300a8d497eafa59af934
ciro matias <matiasciros666@gmail.com>
2026-05-07 11:59:52 -0300

    funciones basicas_js

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+function saludar(){
+    let  nombre = document.getElementById("nombre").value
+    
+    document.getElementById("mensaje").innerText = "hola " + nombre
+}
+
+function sumar(){
+    let num1 = Number(document.getElementById("num1").value)
+    let num2 = Number(document.getElementById("num2").value)
+    document.getElementById("resultado").innerText = num1 + num2
+}
+ 
+let blanco = true
+
+function cambiar(){
+    blanco= !blanco
+    if(blanco==true){
+        document.getElementById("blanco").style.display = "none"
+        document.getElementById("negro").style.display = "block" 
+    }
+    else{
+        document.getElementById("blanco").style.display = "block"
+        document.getElementById("negro").style.display = "none"
+    }
+}
+
+
+let contador = 0
+function sumador(params){
+    contador = contador + params
+    document.getElementById("contador").innerText = contador
+}
+
 console.log("hola mundo");
 //alert("funciona");
 
